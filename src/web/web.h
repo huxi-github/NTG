a1143fa72e398ec302f3ddd0667f04d7ca2b601a
huxi-github <huxi-github@users.noreply.github.com>
2018-09-24 15:41:02 +0800

    Update web.h

--- src/web/web.h
+++ src/web/web.h
@@ -15,6 +15,5 @@ extern void web_behavior(void * user);
 extern int web_initConnection(const char * host);
 extern void web_request(int sockfd, char *msg, size_t msg_size);
 extern ssize_t web_receive(int sockfd, char *buf, size_t buf_size);
-extern struct user_ops web_user_ops;
-
+extern struct user_ops web_user_ops;  //全局变量
 #endif /* WEB_H_ */
